be1e408640eedfba60ea76e6680b22ef040eceb8
Jude <mpoyojude0@gmail.com>
2025-10-30 01:32:54 +0200

    Add files via upload

---
+++ aliases.zsh
@@ -0,0 +1,26 @@
+#my aliases
+
+alias pa='php artisan '
+alias pas='php artisan serve'
+alias crd='composer run dev'
+alias nrd='npm run dev'
+alias nrb='npm run build'
+alias update='sudo apt update && sudo apt upgrade -y'
+alias i='sudo apt install '
+alias ..='cd ..'
+alias ...='cd ../..'
+
+alias zz='z -l'          
+alias zc='z -c'          
+alias zf='z -I' 
+
+alias pj="projet"
+alias pjs="devps"
+alias pjstop="devstop"
+alias fp="findprojet"
+alias lsp="lsprojets"
+
+
+alias pjlaravel="projet $1 laravel"
+alias pjnuxt="projet $1 nuxt"
+alias pjphp="projet $1 php"
